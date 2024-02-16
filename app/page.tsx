ad1feb7761dc1903e638b2a4829ac7ce832d5502
wachiss35 <160213673+wachiss35@users.noreply.github.com>
2024-02-16 14:02:16 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <h1 className="my-14 text-center text-3xl font-bold uppercase 
 tracking-widest">BMI Calculator</h1>
 <a
-          href="https://getbootstrap.com/"
+          href="https://www.youtube.com/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
