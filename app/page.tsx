37ae7a79b0bad0381bbf0f85bea4bde8cea7b75d
wachiss35 <160213673+wachiss35@users.noreply.github.com>
2024-02-16 14:06:26 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-5 lg:p-24">
       <h1 className="my-14 text-center text-3xl font-bold uppercase 
-tracking-widest">BMI Calculator</h1>
+tracking-widest">you are hacked!!</h1>
 <a
           href="https://www.youtube.com/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
