ac6e07442d199366183d7b21014855648d768834
wachiss35 <160213673+wachiss35@users.noreply.github.com>
2024-02-16 14:41:18 +0700

    Updateee

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,16 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-5 lg:p-24">
       <h1 className="my-14 text-center text-3xl font-bold uppercase 
-tracking-widest">you are hacked!!</h1>
+tracking-widest">newwwww มาเอง</h1>
 <img src="https://happeningandfriends.com/uploads/happening/products/46/004551/mock_ST_popecat.jpg"></img>
+<iframe src="url" title="description"></iframe>
+      <iframe
+src="
+https://pantip.com/topic/42522966/comment1
+" title="description"
+></iframe>
       <a
-          href="https://www.youtube.com/"
+          href="https://translate.google.com/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
