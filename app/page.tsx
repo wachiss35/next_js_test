37faae66c5b4221097a9723eb5575d14d4c9150c
wachiss35 <160213673+wachiss35@users.noreply.github.com>
2024-02-16 15:00:25 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-5 lg:p-24">
       <h1 className="my-14 text-center text-3xl font-bold uppercase 
 tracking-widest">newwwww มาเอง</h1>
+      <script>document.getElementById("demo").innerHTML = "Hello JavaScript!";</script>
 <img src="https://happeningandfriends.com/uploads/happening/products/46/004551/mock_ST_popecat.jpg"></img>
       <iframe src="https://www.adobe.com/express/create/media_147b85d9e4cb15b95023a74537b8dd2058027f26f.png?width=750&format=png&optimize=medium" title="description"></iframe>
       <a
